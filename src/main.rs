9ee7d16f2d2deb2d27c2de52721f9565cf3283d4
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-01 21:02:00 -0500

    processing_series_of_items_with_iterators

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-fn call_fn_once<F: FnOnce()>(f: F) { f(); }
-fn call_fn_mut<F: FnMut()>(mut f: F) { f(); }
-fn call_fn<F: Fn()>(f: F) { f(); }
 
 fn main() {
-    let name = String::from("Rust");
+    let v1 = vec![1, 2, 3];
 
-    let say_hi = || println!("Hi, {}", name); // Fn
-    call_fn(say_hi);      // ok
-    call_fn_mut(say_hi);  // ok — Fn is also FnMut
-    call_fn_once(say_hi); // ok — Fn is also FnOnce
+    let v1_iter = v1.iter();
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
 }
