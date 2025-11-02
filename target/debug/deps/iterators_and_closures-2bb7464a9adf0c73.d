895271542235d89ecc74d54589f5e96e40c27fc2
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-02 01:08:32 -0500

    iter_trait_and_next_method

---
+++ target/debug/deps/iterators_and_closures-2bb7464a9adf0c73.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-2bb7464a9adf0c73: src/main.rs
+
+/Users/admin/Documents/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-2bb7464a9adf0c73.d: src/main.rs
+
+src/main.rs:
