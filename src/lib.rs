c2d822d1c62ed82360f43e854816d119bafb85da
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-02 19:05:05 -0600

    refactoring_by_closures_and_iterators_config_project2

--- src/lib.rs
+++ src/lib.rs
@@ -32,9 +32,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
-        search(&config.query, &contents)
-    } else {
         search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
     };
 
     for line in results {
@@ -51,9 +51,8 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    // let query = query.to_lowercase();
     contents
         .lines()
-        .filter(|line| line.contains(&query.to_lowercase()))
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
 }
